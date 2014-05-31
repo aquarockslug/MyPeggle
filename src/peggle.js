5314e898d535b2f2f68995b4cb80016c15b75321
Elizabeth Maya <lizabwth@gmail.com>
2014-05-31 01:24:11 -0700

    player control

--- src/peggle.js
+++ src/peggle.js
@@ -1,27 +1,27 @@
-var game 		= new Phaser.Game(800, 600, Phaser.AUTO, 'game', { preload: preload, create: create, update: update, render: render  });
+var game 		= new Phaser.Game(380, 500, Phaser.AUTO, 'game', { preload: preload, create: create, update: update, render: render  });
 var div 		= document.getElementById("game");
 var WIDTH 		= div.offsetWidth;
 var HEIGHT 		= div.offsetHeight;
 
+var player = null;
+
 function preload() {
 	game.load.image('heart', 'assets/sprites/heart.png');
+
+	player = new Player(game);
 }
 
 function create() {
 	game.physics.startSystem(Phaser.Physics.P2JS);
 	game.physics.p2.restitution = 0.9;
-	game.physics.p2.gravity.y = 100;
+	game.physics.p2.gravity.y = 300;
 
-	var heart = game.add.sprite(WIDTH/2, HEIGHT/2, 'heart');
-	game.physics.p2.enable(heart);
-	heart.body.setCircle(5);
-
+	player.create();
 }
 
 function update() {
-	
+	player.update();
 }
 
 function render() {
-
 }
---
+++ src/player.js
@@ -0,0 +1,37 @@
+function Projectile(posX, posY){
+
+}
+
+function Player(game) {
+	// preload
+	game.load.image('player', 'assets/sprites/player.png');
+	game.load.image('heart', 'assets/sprites/heart.png');
+
+	// http://www.html5gamedevs.com/topic/1764-how-to-capture-a-touch-event-on-mobile-device/?p=12250
+	game.input.onDown.add(fire, this);
+
+	// create
+	this.create = create;
+	var playerSprite = null;
+	function create(){
+		playerSprite = game.add.sprite(0, 0, 'player');
+		playerSprite.anchor.x = .5;
+		playerSprite.anchor.y = 0;
+	}
+
+	// update
+	this.update = update;
+	function update() {
+		playerSprite.x = game.input.mousePointer.x;
+	}
+
+	// fire projectile
+	this.fire = fire;
+	function fire() {
+		var heart = game.add.sprite(0, 0, 'heart');
+		heart.x = playerSprite.x;
+		heart.y = 14;
+		game.physics.p2.enable(heart, Phaser.Physics.P2JS); // debug draw code?
+		heart.body.setCircle(heart.width/2);
+	}
+}
