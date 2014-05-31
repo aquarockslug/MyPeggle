a3daaca0c3af07decd8126f8c01396e56a121f76
Elizabeth Maya <lizabwth@gmail.com>
2014-05-30 19:23:01 -0700

    First physics test

--- src/peggle.js
+++ src/peggle.js
@@ -1,15 +1,27 @@
-var game 		= new Phaser.Game(800, 600, Phaser.AUTO, 'game', { preload: preload, create: create });
+var game 		= new Phaser.Game(800, 600, Phaser.AUTO, 'game', { preload: preload, create: create, update: update, render: render  });
 var div 		= document.getElementById("game");
 var WIDTH 		= div.offsetWidth;
 var HEIGHT 		= div.offsetHeight;
 
 function preload() {
-	game.load.image('ayy', 'assets/sprites/ayy.jpg');
 	game.load.image('heart', 'assets/sprites/heart.png');
 }
 
 function create() {
-	game.add.sprite(0, 0, 'ayy');
+	game.physics.startSystem(Phaser.Physics.P2JS);
+	game.physics.p2.restitution = 0.9;
+	game.physics.p2.gravity.y = 100;
+
 	var heart = game.add.sprite(WIDTH/2, HEIGHT/2, 'heart');
-	heart.scale.setTo(2,2);
+	game.physics.p2.enable(heart);
+	heart.body.setCircle(5);
+
+}
+
+function update() {
+	
+}
+
+function render() {
+
 }
