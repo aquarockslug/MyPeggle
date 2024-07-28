bde8200874d97857b4cdb74483398d0463864147
aqua <aquarockslug@gmail.com>
2024-07-28 11:51:54 -0500

    aim projectile

--- src/player.js
+++ src/player.js
@@ -1,37 +1,29 @@
-function Projectile(posX, posY){
-
-}
-
 function Player(game) {
-	// preload
-	game.load.image('player', 'assets/sprites/player.png');
-	game.load.image('heart', 'assets/sprites/heart.png');
+        // preload
+        game.load.image('player', 'assets/sprites/player.png');
+        game.load.image('heart', 'assets/sprites/heart.png');
+        game.input.onDown.add(() => Projectile(), this);
+        var playerSprite;
 
-	// http://www.html5gamedevs.com/topic/1764-how-to-capture-a-touch-event-on-mobile-device/?p=12250
-	game.input.onDown.add(fire, this);
+        this.create = function create() {
+                playerSprite = game.add.sprite(game.width / 2, 0, 'player');
+                playerSprite.anchor = {
+                        x: 0.5,
+                        y: 0
+                };
+        }
 
-	// create
-	var playerSprite;
-	this.create = create;
-	function create(){
-		playerSprite = game.add.sprite(0, 0, 'player');
-		playerSprite.anchor.x = .5;
-		playerSprite.anchor.y = 0;
-	}
-
-	// update
-	this.update = update;
-	function update() {
-		playerSprite.x = game.input.mousePointer.x;
-	}
-
-	// fire projectile
-	this.fire = fire;
-	function fire() {
-		var heart = game.add.sprite(0, 0, 'heart');
-		heart.x = playerSprite.x;
-		heart.y = 14;
-		game.physics.p2.enable(heart, Phaser.Physics.P2JS); // debug draw code?
-		heart.body.setCircle(heart.width/2);
-	}
+        this.update = function update() {
+                playerSprite.rotation = mouseX() * 2;
+        }
 }
+
+function Projectile(initVelocity = [1000, 0]) {
+        var heart = game.add.sprite(game.width / 2, 14, 'heart');
+        game.physics.p2.enable(heart, Phaser.Physics.P2JS);
+        heart.body.setCircle(heart.width / 2);
+        heart.body.data.force = [mouseX() * initVelocity[0], -10]
+}
+
+// returns a number between -0.5 and 0.5
+mouseX = () => 0.5 - game.input.mousePointer.x / game.width
