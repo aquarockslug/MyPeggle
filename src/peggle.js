d782ee26697719446539e532d2540c2f69d0cef9
aqua <aquarockslug@gmail.com>
2024-08-10 22:50:04 -0500

    pegs

--- src/peggle.js
+++ src/peggle.js
@@ -1,27 +1,67 @@
-var game 		= new Phaser.Game(380, 500, Phaser.AUTO, 'game', { preload: preload, create: create, update: update, render: render  });
-var div 		= document.getElementById("game");
-var WIDTH 		= div.offsetWidth;
-var HEIGHT 		= div.offsetHeight;
+var div = document.getElementById("game");
+var WIDTH = div.offsetWidth;
+var HEIGHT = div.offsetHeight;
+var game = new Phaser.Game(WIDTH, HEIGHT, Phaser.AUTO, 'game', {
+        preload: preload,
+        create: create,
+        update: update,
+        render: render
+});
 
 var player;
+var pegs;
 
 function preload() {
-	game.load.image('heart', 'assets/sprites/heart.png');
+        game.load.image('heart', 'assets/sprites/heart.png');
+        game.load.image('background', 'assets/background.jpeg');
 
-	player = new Player(game);
+        player = new Player(game);
 }
 
 function create() {
-	game.physics.startSystem(Phaser.Physics.P2JS);
-	game.physics.p2.restitution = 0.9;
-	game.physics.p2.gravity.y = 300;
+        game.physics.startSystem(Phaser.Physics.P2JS);
+        game.physics.p2.restitution = 0.9;
+        game.physics.p2.gravity.y = 600;
 
-	player.create();
+        game.add.image(0, 0, 'background')
+        player.create();
+        var pegs = Pegs();
 }
+
+function Pegs() {
+        var pegs = []
+        var spacing = [100, 100]
+        var globalTopLeft = [140, HEIGHT / 4]
+
+        function Peg(x, y) {
+                var peg = game.add.sprite(x, y, 'heart');
+                game.physics.p2.enable(peg, Phaser.Physics.P2JS);
+                peg.body.static = true
+                peg.body.setCircle(peg.width / 2);
+                return peg
+        }
+
+        function createGrid(topLeft) {
+                for (let c = 0; c <= 4; c++)
+                        for (let r = 0; r <= 4; r++)
+                                pegs.push(Peg(
+                                        topLeft[0] + c * spacing[0],
+                                        topLeft[1] + r * spacing[1],
+                                ))
+        }
+
+        createGrid([globalTopLeft[0], globalTopLeft[1]]);
+        createGrid([globalTopLeft[0] + spacing[0] / 2,
+                globalTopLeft[1] + spacing[1] / 2
+        ]);
+
+        return pegs
+}
+
+
 
 function update() {
-	player.update();
+        player.update();
 }
 
-function render() {
-}
+function render() {}
--- src/player.js
+++ src/player.js
@@ -2,8 +2,9 @@ function Player(game) {
         // preload
         game.load.image('player', 'assets/sprites/player.png');
         game.load.image('heart', 'assets/sprites/heart.png');
-        game.input.onDown.add(() => Projectile(), this);
+        game.input.onDown.add(() => this.shoot(), this);
         var playerSprite;
+        var balls = [];
 
         this.create = function create() {
                 playerSprite = game.add.sprite(game.width / 2, 0, 'player');
@@ -15,15 +16,19 @@ function Player(game) {
 
         this.update = function update() {
                 playerSprite.rotation = mouseX() * 2;
+                balls.forEach((b) => {
+                        if (b.position.y > HEIGHT - b.height) b.destroy()
+                })
+        }
+
+        this.shoot = (initVelocity = [1000, 0]) => {
+                var ball = game.add.sprite(game.width / 2, 14, 'heart');
+                game.physics.p2.enable(ball, Phaser.Physics.P2JS);
+                ball.body.setCircle(ball.width / 2);
+                ball.body.data.force = [mouseX() * initVelocity[0], -10]
+                balls.push(ball)
         }
 }
 
-function Projectile(initVelocity = [1000, 0]) {
-        var heart = game.add.sprite(game.width / 2, 14, 'heart');
-        game.physics.p2.enable(heart, Phaser.Physics.P2JS);
-        heart.body.setCircle(heart.width / 2);
-        heart.body.data.force = [mouseX() * initVelocity[0], -10]
-}
-
-// returns a number between -0.5 and 0.5
+// returns a number between -0.5 and 0.5 representing the mouse position
 mouseX = () => 0.5 - game.input.mousePointer.x / game.width
