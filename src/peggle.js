45bd36e4ff15f4c569adffa80c2106f86625f0ea
aqua <aquarockslug@gmail.com>
2024-08-12 02:46:05 -0500

    refactor

--- src/peggle.js
+++ src/peggle.js
@@ -1,19 +1,17 @@
+var player;
+var pegs;
 var div = document.getElementById("game");
 var WIDTH = div.offsetWidth;
 var HEIGHT = div.offsetHeight;
 var game = new Phaser.Game(WIDTH, HEIGHT, Phaser.AUTO, 'game', {
         preload: preload,
         create: create,
-        update: update,
-        render: render
+        update: () => player.update(),
 });
 
-var player;
-var pegs;
-
 function preload() {
-        game.load.image('heart', 'assets/sprites/heart.png');
         game.load.image('background', 'assets/background.jpeg');
+        game.load.image('red_peg', 'assets/sprites/red_peg_small.png');
 
         player = new Player(game);
 }
@@ -25,43 +23,32 @@ function create() {
 
         game.add.image(0, 0, 'background')
         player.create();
-        var pegs = Pegs();
+        var pegs = Pegs().filter((p) => {
+                if (Math.random() > 0.5) p.destroy()
+                //TODO: assign goal pegs
+                return p.visible
+        })
+        console.log(pegs)
 }
 
-function Pegs() {
-        var pegs = []
-        var spacing = [100, 100]
-        var globalTopLeft = [140, HEIGHT / 4]
-
-        function Peg(x, y) {
-                var peg = game.add.sprite(x, y, 'heart');
+function Pegs(topLeft = [140, HEIGHT / 4], spacing = [100, 100]) {
+        function Peg(x, y, peg = game.add.sprite(x, y, 'red_peg')) {
                 game.physics.p2.enable(peg, Phaser.Physics.P2JS);
                 peg.body.static = true
                 peg.body.setCircle(peg.width / 2);
                 return peg
         }
 
-        function createGrid(topLeft) {
+        function Grid(localTopLeft, pegs = []) {
                 for (let c = 0; c <= 4; c++)
                         for (let r = 0; r <= 4; r++)
                                 pegs.push(Peg(
-                                        topLeft[0] + c * spacing[0],
-                                        topLeft[1] + r * spacing[1],
+                                        localTopLeft[0] + c * spacing[0],
+                                        localTopLeft[1] + r * spacing[1],
                                 ))
+                return pegs
         }
 
-        createGrid([globalTopLeft[0], globalTopLeft[1]]);
-        createGrid([globalTopLeft[0] + spacing[0] / 2,
-                globalTopLeft[1] + spacing[1] / 2
-        ]);
-
-        return pegs
+        return Grid([topLeft[0], topLeft[1]]).concat(
+                Grid([topLeft[0] + spacing[0] / 2, topLeft[1] + spacing[1] / 2]))
 }
-
-
-
-function update() {
-        player.update();
-}
-
-function render() {}
--- src/player.js
+++ src/player.js
@@ -1,10 +1,8 @@
 function Player(game) {
-        // preload
         game.load.image('player', 'assets/sprites/player.png');
         game.load.image('heart', 'assets/sprites/heart.png');
-        game.input.onDown.add(() => this.shoot(), this);
         var playerSprite;
-        var balls = [];
+        var ball = null;
 
         this.create = function create() {
                 playerSprite = game.add.sprite(game.width / 2, 0, 'player');
@@ -16,18 +14,20 @@ function Player(game) {
 
         this.update = function update() {
                 playerSprite.rotation = mouseX() * 2;
-                balls.forEach((b) => {
-                        if (b.position.y > HEIGHT - b.height) b.destroy()
-                })
+                if (ball && ball.position.y > HEIGHT - ball.height) ball.destroy()
         }
 
+        this.canShoot = () => !ball || !ball.visible
         this.shoot = (initVelocity = [1000, 0]) => {
                 var ball = game.add.sprite(game.width / 2, 14, 'heart');
                 game.physics.p2.enable(ball, Phaser.Physics.P2JS);
                 ball.body.setCircle(ball.width / 2);
                 ball.body.data.force = [mouseX() * initVelocity[0], -10]
-                balls.push(ball)
+                return ball
         }
+        game.input.onDown.add(() => {
+                if (this.canShoot()) ball = this.shoot()
+        }, this);
 }
 
 // returns a number between -0.5 and 0.5 representing the mouse position
