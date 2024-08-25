0e7f9bb3f17ee492e81b7ae02cfce58ff0d3a24e
aqua <aquarockslug@gmail.com>
2024-08-25 15:11:10 -0500

    sprite images

--- src/peggle.js
+++ src/peggle.js
@@ -3,52 +3,58 @@ var pegs;
 var div = document.getElementById("game");
 var WIDTH = div.offsetWidth;
 var HEIGHT = div.offsetHeight;
-var game = new Phaser.Game(WIDTH, HEIGHT, Phaser.AUTO, 'game', {
-        preload: preload,
-        create: create,
-        update: () => player.update(),
+var game = new Phaser.Game(WIDTH, HEIGHT, Phaser.AUTO, "game", {
+  preload: preload,
+  create: create,
+  update: () => player.update(),
 });
 
 function preload() {
-        game.load.image('background', 'assets/background.jpeg');
-        game.load.image('red_peg', 'assets/sprites/red_peg_small.png');
+  game.load.image("background", "assets/background/1.jpg");
+  game.load.image("orange", "assets/sprites/orange.png");
 
-        player = new Player(game);
+  player = new Player(game);
 }
 
 function create() {
-        game.physics.startSystem(Phaser.Physics.P2JS);
-        game.physics.p2.restitution = 0.9;
-        game.physics.p2.gravity.y = 600;
+  game.physics.startSystem(Phaser.Physics.P2JS);
+  game.physics.p2.restitution = 0.9;
+  game.physics.p2.gravity.y = 600;
 
-        game.add.image(0, 0, 'background')
-        player.create();
-        var pegs = Pegs().filter((p) => {
-                if (Math.random() > 0.5) p.destroy()
-                //TODO: assign goal pegs
-                return p.visible
-        })
-        console.log(pegs)
+  var background = game.add.image(0, 0, "background");
+  player.create();
+  var destroyChance = 1;
+  var pegs = Pegs()
+    .sort((a, b) => a.position.y - b.position.y)
+    .filter((p) => {
+      if (Math.random() < destroyChance) p.destroy();
+      //TODO: assign goal pegs
+      destroyChance -= 0.0125; // chance goes down higher up
+      return p.visible; // only save if visible
+    });
+  console.log(pegs);
 }
 
 function Pegs(topLeft = [140, HEIGHT / 4], spacing = [100, 100]) {
-        function Peg(x, y, peg = game.add.sprite(x, y, 'red_peg')) {
-                game.physics.p2.enable(peg, Phaser.Physics.P2JS);
-                peg.body.static = true
-                peg.body.setCircle(peg.width / 2);
-                return peg
-        }
+  function Peg(x, y, peg = game.add.sprite(x, y, "orange")) {
+    game.physics.p2.enable(peg, Phaser.Physics.P2JS);
+    peg.body.static = true;
+    peg.body.setCircle(peg.width / 2);
+    return peg;
+  }
 
-        function Grid(localTopLeft, pegs = []) {
-                for (let c = 0; c <= 4; c++)
-                        for (let r = 0; r <= 4; r++)
-                                pegs.push(Peg(
-                                        localTopLeft[0] + c * spacing[0],
-                                        localTopLeft[1] + r * spacing[1],
-                                ))
-                return pegs
-        }
-
-        return Grid([topLeft[0], topLeft[1]]).concat(
-                Grid([topLeft[0] + spacing[0] / 2, topLeft[1] + spacing[1] / 2]))
+  function Grid(localTopLeft, pegs = []) {
+    for (let c = 0; c <= 4; c++)
+      for (let r = 0; r <= 4; r++)
+        pegs.push(
+          Peg(
+            localTopLeft[0] + c * spacing[0],
+            localTopLeft[1] + r * spacing[1],
+          ),
+        );
+    return pegs;
+  }
+  return Grid([topLeft[0], topLeft[1]]).concat(
+    Grid([topLeft[0] + spacing[0] / 2, topLeft[1] + spacing[1] / 2]),
+  );
 }
--- src/player.js
+++ src/player.js
@@ -1,34 +1,36 @@
 function Player(game) {
-        game.load.image('player', 'assets/sprites/player.png');
-        game.load.image('heart', 'assets/sprites/heart.png');
-        var playerSprite;
-        var ball = null;
+  game.load.image("player", "assets/sprites/player.png");
+  game.load.image("ball", "assets/sprites/ball.png");
+  var playerSprite;
+  var ball = null;
 
-        this.create = function create() {
-                playerSprite = game.add.sprite(game.width / 2, 0, 'player');
-                playerSprite.anchor = {
-                        x: 0.5,
-                        y: 0
-                };
-        }
+  this.create = function create() {
+    playerSprite = game.add.sprite(game.width / 2, 0, "player");
+    playerSprite.anchor = {
+      x: 0.5,
+      y: 0,
+    };
+  };
 
-        this.update = function update() {
-                playerSprite.rotation = mouseX() * 2;
-                if (ball && ball.position.y > HEIGHT - ball.height) ball.destroy()
-        }
+  this.update = function update() {
+    playerSprite.rotation = mouseX() * 2;
+    if (ball && ball.position.y > HEIGHT - ball.height) ball.destroy();
+  };
 
-        this.canShoot = () => !ball || !ball.visible
-        this.shoot = (initVelocity = [1000, 0]) => {
-                var ball = game.add.sprite(game.width / 2, 14, 'heart');
-                game.physics.p2.enable(ball, Phaser.Physics.P2JS);
-                ball.body.setCircle(ball.width / 2);
-                ball.body.data.force = [mouseX() * initVelocity[0], -10]
-                return ball
-        }
-        game.input.onDown.add(() => {
-                if (this.canShoot()) ball = this.shoot()
-        }, this);
+  this.canShoot = () => !ball || !ball.visible;
+  this.shoot = (initVelocity = [1000, 0]) => {
+    var ball = game.add.sprite(game.width / 2, 14, "ball");
+    game.physics.p2.enable(ball, Phaser.Physics.P2JS);
+    ball.body.setCircle(ball.width / 2);
+    ball.body.data.force = [mouseX() * initVelocity[0], -10];
+    var checkGroup = game.add.group();
+    checkGroup.add(simpleCollisionSprite);
+    return ball;
+  };
+  game.input.onDown.add(() => {
+    if (this.canShoot()) ball = this.shoot();
+  }, this);
 }
 
 // returns a number between -0.5 and 0.5 representing the mouse position
-mouseX = () => 0.5 - game.input.mousePointer.x / game.width
+mouseX = () => 0.5 - game.input.mousePointer.x / game.width;
